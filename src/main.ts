bb6f47b714f4b3091dde1ff1787858fd248f7d02
anarckk <anarckk@gmail.com>
2024-05-25 17:02:17 +0800

    引入varlet

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Varlet from '@varlet/ui'
+import '@varlet/ui/es/style'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Varlet)
 
 app.mount('#app')
