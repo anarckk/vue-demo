1e28357e07c67b9b0bd71ab0dc83f3978d1a86b7
anarckk <anarckk@gmail.com>
2024-05-30 18:11:58 +0800

    根据路由选择哪个tab

--- src/views/EmptyView.vue
+++ src/views/EmptyView.vue
@@ -2,16 +2,31 @@
 import { ref } from "vue";
 import { RouterView } from "vue-router";
 import { useRouter } from 'vue-router';
+import { onBeforeMount } from 'vue'
 
-const tabsArr = ref(['/default', '/test']);
+const tabsArr = ref([
+  {path: '/default', name: "默认"}, 
+  {path: '/test', name: "test"},
+]);
 
 const active = ref(0);
 const router = useRouter()
-console.log('router',router.currentRoute.value);
 
 function _change () {
   console.log('fuck', active.value);
+  var link = tabsArr.value[active.value].path;
+  console.log(link);
+  router.push(link);
 }
+onBeforeMount(() => {
+  const fullpath: string = router.currentRoute.value.fullPath;
+  for (let i = 0; i < tabsArr.value.length; i++) {
+    if (fullpath.startsWith(tabsArr.value[i].path)) {
+      active.value = i;
+      break;
+    }
+  }
+})
 </script>
 
 <template>
@@ -24,11 +39,7 @@ function _change () {
         v-model:active="active"
         @change="_change"
       >
-        <var-tab>default{{ active }}</var-tab>
-        <var-tab>选项2</var-tab>
-        <var-tab>选项3</var-tab>
-        <var-tab>选项3</var-tab>
-        <var-tab>选项3</var-tab>
+        <var-tab v-for="item in tabsArr">{{ item.name }}</var-tab>
       </var-tabs>
     </div>
 
@@ -51,7 +62,7 @@ function _change () {
   position: fixed;
 }
 .tabs-example-vertical {
-  height: 240px;
+  /* height: 240px; */
 }
 .card-wrap {
   padding: 20px;
